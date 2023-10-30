1f75df3d721eff019bfa5d3d3fc7883ac05734e2
Derek Mercedes <mercedes.derek@gmail.com>
2023-10-29 18:42:43 -0600

    Multiplies two fixed values in the most verbose way possible

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr){
+    return [arr[0], arr[1]]
+}
+
+function returnLastTwoDrivers(arr){
+    return [arr[arr.length-2], arr[arr.length-1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    
+    if(num === 2){
+        return fareDoubler
+    } else if(num ===3){
+        return fareTripler
+    } else if(num ===4){
+        return fareQuadrupler
+    } else if(num ===5){
+        return fareQuintupler
+    }
+
+}
+
+function fareDoubler(fare){
+    return fare*2
+
+}
+
+function fareTripler(fare){
+    return fare*3
+}
+
+function fareQuadrupler(fare){
+    return fare*4
+}
+
+function fareQuintupler(fare){
+    return fare*5
+}
+
+function selectDifferentDrivers(arr, which){
+    return which(arr)
+}
